e19431d4822cb6c27e2327d565ed02673cb887c9
willyjgh <willyjgh@gmail.com>
2024-01-23 14:28:17 +0700

    first commit

--- backend/backend_product.js
+++ backend/backend_product.js
@@ -1,28 +1,28 @@
-const express = require('express');
-const bodyParser = require('body-parser');
+const express = require("express");
+const bodyParser = require("body-parser");
 
 const app = express();
-const PORT = 3000;
+const PORT = 3001;
 
 app.use(bodyParser.json());
 
 const products = [
-  { id: 1, name: 'Sendal' },
-  { id: 2, name: 'Sepatu' },
-  { id: 3, name: 'baju' },
+  { id: 1, name: "Sendal" },
+  { id: 2, name: "Sepatu" },
+  { id: 3, name: "baju" },
 ];
 
-app.get('/api/products', (req, res) => {
+app.get("/api/products", (req, res) => {
   res.json(products);
 });
 
-app.get('/api/products/:id', (req, res) => {
-  const user = products.find(u => u.id === parseInt(req.params.id));
-  if (!user) return res.status(404).send('products not found');
+app.get("/api/products/:id", (req, res) => {
+  const user = products.find((u) => u.id === parseInt(req.params.id));
+  if (!user) return res.status(404).send("products not found");
   res.json(user);
 });
 
-app.post('/api/products', (req, res) => {
+app.post("/api/products", (req, res) => {
   const user = {
     id: products.length + 1,
     name: req.body.name,
--- backend/backend_user.js
+++ backend/backend_user.js
@@ -1,28 +1,28 @@
-const express = require('express');
-const bodyParser = require('body-parser');
+const express = require("express");
+const bodyParser = require("body-parser");
 
 const app = express();
-const PORT = 4000;
+const PORT = 4001;
 
 app.use(bodyParser.json());
 
 const users = [
-  { id: 1, name: 'John Doe' },
-  { id: 2, name: 'Jane Smith' },
-  { id: 3, name: 'Bob Johnson' },
+  { id: 1, name: "John Doe" },
+  { id: 2, name: "Jane Smith" },
+  { id: 3, name: "Bob Johnson" },
 ];
 
-app.get('/api/users', (req, res) => {
+app.get("/api/users", (req, res) => {
   res.json(users);
 });
 
-app.get('/api/users/:id', (req, res) => {
-  const user = users.find(u => u.id === parseInt(req.params.id));
-  if (!user) return res.status(404).send('User not found');
+app.get("/api/users/:id", (req, res) => {
+  const user = users.find((u) => u.id === parseInt(req.params.id));
+  if (!user) return res.status(404).send("User not found");
   res.json(user);
 });
 
-app.post('/api/users', (req, res) => {
+app.post("/api/users", (req, res) => {
   const user = {
     id: users.length + 1,
     name: req.body.name,
